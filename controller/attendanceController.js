576cd26af47371239e3609a93c0c55f75a49f09e
pativatkongsawat <pativat.k@kkumail.com>
2024-09-10 16:40:58 +0700

    add attendance

--- controller/attendanceController.js
+++ controller/attendanceController.js
@@ -1,1 +1,99 @@
+const { get } = require('mongoose')
 const  Attendance= require('../schema/attendanceSchema')
+
+exports.getAttendance = async (req , res) => {
+
+
+    try{
+
+        const getattendance = await Attendance.find()
+
+        if(!getattendance){
+
+            return res.status(404).json({
+
+                message: 'Attendance not found'
+            
+            })
+
+            
+        }
+        return res.status(200).json({
+
+            Attendance : getattendance
+
+        })
+
+
+
+    }catch(err){
+        
+        return res.status(500).json({
+
+            message: err.message
+
+        })
+
+    }
+
+}
+
+exports.getAttendanceBy = async(req , res) => {
+
+
+    const {student_id} = req.user
+
+    try{
+
+        const getattendance = await Attendance.find({student_id})
+
+        if(!getattendance){
+
+            return res.status(404).json({
+
+                message : 'Attendance not found'
+
+            })
+
+            
+        }
+        return res.status(200).json({
+
+            Attendance: getattendance
+
+        })
+
+
+    }catch(err){
+
+        return res.status(500).json({
+
+            message: err.message
+
+        })
+
+    }
+
+
+}
+
+exports.createAttendance = async (req , res) =>{
+
+
+    const {student_id , student_fname : first_name , student_lastname : last_name } = req
+
+    try{
+
+
+    }catch(err){
+
+        return res.status(500).json({
+
+            message:err.message
+
+        })
+
+    }
+
+
+}
--- schema/attendanceSchema.js
+++ schema/attendanceSchema.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const attendanceSchema = new mongoose.Schema({
   
         
-        enrollment_id:{type: 'string' , required: true},
+        course_code:{type: 'string' , required: true},
+        student_id:{type: 'string' , required: true},
+        student_fname:{type: 'string', required : true},
+        studnet_lname:{type: 'string', required : true},
         date :{type:'date',required: true},
-        status: {type:'string' , required: true}
-        
-    
-});
-
-module.exports = mongoose.model('Attendance' , attendanceSchema)
+        status: {type:'string' , required: true},
+        deletedAt: { type: Date } 
+}, { timestamps: true });
+module.exports = mongoose.model('Attendance' , attendanceSchema , 'attendances')
